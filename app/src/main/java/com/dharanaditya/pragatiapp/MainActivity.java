4a5503584b1d188d244642579110f58476dd459d
Dharan Aditya <dharan.aditya@gmail.com>
2016-11-29 21:04:36 +0530

    ADDED "Splash_Screen"

--- app/src/main/java/com/dharanaditya/pragatiapp/MainActivity.java
+++ app/src/main/java/com/dharanaditya/pragatiapp/MainActivity.java
@@ -10,24 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
 }
---
+++ app/src/main/java/com/dharanaditya/pragatiapp/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.dharanaditya.pragatiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
