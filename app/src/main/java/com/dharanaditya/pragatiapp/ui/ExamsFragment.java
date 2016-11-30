f71e66ee264edf88a5c8cdc23e84d4c0b63cc547
Dharan Aditya <dharan.aditya@gmail.com>
2016-11-30 22:09:26 +0530

    Clean Code

--- app/src/main/java/com/dharanaditya/pragatiapp/ui/ExamsFragment.java
+++ app/src/main/java/com/dharanaditya/pragatiapp/ui/ExamsFragment.java
@@ -4,15 +4,11 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.dharanaditya.pragatiapp.Model.Exams;
 import com.dharanaditya.pragatiapp.R;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -29,15 +25,6 @@ public class ExamsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.fragment_exams,container,false);
-        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.examsRCV);
-        FirebaseRecyclerAdapter<Exams,ViewHolder> adapter = new FirebaseRecyclerAdapter<Exams, ViewHolder>(Exams.class,R.layout.exam_item,ViewHolder.class,reference) {
-            @Override
-            protected void populateViewHolder(ViewHolder viewHolder, Exams model, int position) {
-                viewHolder.branch.setText(model.getBranch());
-                viewHolder.title.setText(model.getTitle());
-            }
-        };
-        recyclerView.setAdapter(adapter);
         return v;
     }
 
@@ -51,13 +38,5 @@ public class ExamsFragment extends Fragment {
         super.onStart();
     }
 
-    private class ViewHolder extends RecyclerView.ViewHolder{
-        TextView title,branch;
-        public ViewHolder(View itemView) {
-            super(itemView);
-            title = (TextView) itemView.findViewById(R.id.exam_title);
-            branch = (TextView) itemView.findViewById(R.id.exam_branch);
-        }
-    }
 }
 
